8ec0ee88bb2a20b34b9560abf86083b003764826
rishabhpandey202 <rishipandeyy0039@gmail.com>
2022-05-04 20:59:02 +0530

    footer and navbar exported please import

---
+++ components/footer.js
@@ -0,0 +1,151 @@
+
+function footer_main(){
+    return `
+    
+    <div id="footer">
+
+        <div id="signup_para">
+            <h3>Sign Up for Email</h3>
+            <p>Receive early access to new arrivals, sales, exclusive content, events and much more!</p>
+        </div>
+        <div id="sign_input">
+            <h3>Email Address*</h3>
+            <input type="text">
+            <button>SUBMIT</button>
+        </div>
+    </div>
+    <div id="para">
+        <p>By signing up, you will receive Anthropologie offers, promotions and other commercial messages. You are also
+            agreeing to Anthropologie’s <a href="#">Privacy Policy</a> . You may unsubscribe at any time.</p>
+    </div>
+    <hr>
+
+    <div id="footer2">
+
+        <div id="main">
+
+            <div>
+                <ul>
+                    <li>
+                        <h3>Help</h3>
+                    </li>
+                    <li>Track Your Order</li>
+                    <li>Start a Return Or Exchange </li>
+
+                    <li>Return & Exchange</li>
+                    <li>Shipping</li>
+                    <li>Cutomer Service</li>
+                    <li>Current Promotions</li>
+                    <li>Product Recalls</li>
+                    <li>COVID-19 Update</li>
+                </ul>
+
+            </div>
+
+            <div>
+                <ul>
+                    <li>
+                        <h3>About Us</h3>
+                    </li>
+                    <li>Our Story</li>
+                    <li>Events </li>
+
+                    <li>A Greater Good</li>
+                    <li>Diversity & Inclusion</li>
+                    <li>Stories</li>
+
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    <li>
+                        <h3>Services</h3>
+                    </li>
+                    <li>Anthro Perks</li>
+                    <li>Gift Cards</li>
+
+                    <li>Trade Membership</li>
+                    <li>Furniture Guides & Services</li>
+                    <li>Store Pickup & Collection Points</li>
+
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    <li>
+                        <h3>Connect</h3>
+                    </li>
+
+                    <li>Contact Us</li>
+                    <li>Stay Connected</li>
+
+                    <li> Careers </li>
+                    <li>Styling Services</li>
+                    <li>Request A Catalog</li>
+
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    <li></li>
+                    <li style=" color: #52889b;"> <span><i class="fa fa-map-marker" aria-hidden="true"
+                                style="font-size:24px"></i></span> Store Locator</li>
+                    <li style=" color: #52889b;"> <span><i class="fa fa-envelope-o" style="font-size:24px"></i></span>
+                        Get Email</li>
+
+
+
+                </ul>
+
+            </div>
+        </div>
+
+        <div id="twitter">
+            <span> <img
+                    src="https://images.ctfassets.net/5de70he6op10/4gWq1G4qLLgmqlNeHfzV2T/55298745127c36c5876f4013434c0c37/Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917_Copy_2.svg"
+                    alt=""> <a href=""></a></span>
+            <span> <i class="fa fa-pinterest" style="font-size:24px;color: #52889b"></i></span>
+            <span><i class="fa fa-instagram" style="font-size:36px;color: #52889b;"></i></span>
+            <span><i class="fa fa-facebook-official" style="font-size:36px;color: #52889b;"></i></span>
+            <span><i class="fa fa-twitter" style="font-size:24px;color: #52889b;"></i></span>
+
+        </div>
+
+        <div id="shine_img"> <img
+                src="https://images.ctfassets.net/5de70he6op10/5dQGHO8D1YIWYeqqKOSMAG/e8b853d2a7f1967ce2b4039f5b8b94bf/032122_MothersDay_Gifting_SiteFooter_LS.jpg"
+                alt=""></div>
+
+        <div>
+            <ul id="usa">
+                <li>US |</li>
+                <li>France |</li>
+                <li>Germany |</li>
+                <li>UK </li>
+            </ul>
+        </div>
+
+        <div>
+
+            <ul id="term">
+                <li class="term_condition_blueColor">Privacy Policy |</li>
+                <li class="term_condition_blueColor">Term Of Use|</li>
+                <li class="term_condition_blueColor">CA Transperency|</li>
+                <li class="term_condition_blueColor">Accesibility </li>
+                <li class="term_condition_blueColor">URBN.com</li>
+                <li >For CA Residents</li>
+                <li style="margin-left: 360px;">2022 URBN.com. All Rights Reserved.</li>
+            </ul>
+        </div>
+
+
+
+
+    </div>
+
+
+    `
+} 
+export default footer_main;
