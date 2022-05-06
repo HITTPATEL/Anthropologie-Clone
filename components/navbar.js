305c5be5e45235ddf007700cb38ddce0beaa1311
rishabhpandey202 <rishipandeyy0039@gmail.com>
2022-05-06 21:35:14 +0530

    navbar updated

--- components/navbar.js
+++ components/navbar.js
@@ -25,14 +25,14 @@ function navbar_main(){
 <div id="search">
 <div id="first">
     <div id="a">
-        <h6> <a href="#">
+        <h6> <a href="index.html">
                 <img src="https://images.ctfassets.net/5de70he6op10/53ZOE4rRqrxcvv0hg2eSLV/a849085e5f600c618132be2475017746/anthro-logo.svg"
                     alt="">
             </a>
         </h6>
     </div>
     <div id="b">
-        <h6> <a href="#">
+        <h6> <a href="home.html">
                 <img src="https://images.ctfassets.net/5de70he6op10/y1O9tEDOvCGliGUvT9RoJ/eb8ab4734c5e3778b8b61da70ed441ac/AL_NewLogo_2.svg"
                     alt="">
             </a>
@@ -56,17 +56,17 @@ function navbar_main(){
 
 <div id="toggle">
 <div>New!</div>
-<div>Gifts foer Mom</div>
-<div> <a href="#" style="text-decoration: none; color:black" >Bedding</a>
+<div>Gifts for Mom</div>
+<div> <a href="bed.html" style="text-decoration: none; color:black" >Bedding</a>
 </div>
 <div>Clothing</div>
 <div>Shoes</div>
 <div>Accessories</div>
-<div>home&Furm</div>
-<div>home&Furm</div>
-<div>home&Furm</div>
-<div>home&Furm</div>
-<div>home&Furm</div>
+<div>Home & Furniture</div>
+<div>Beauty & Wellness</div>
+<div>Garden & Outdoor</div>
+<div>Weddings</div>
+<div>Sale</div>
 </div>
 <hr>
 
