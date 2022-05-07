54eecdd6357a57179a0aa0c9db0fdc89535ed811
abhm1999 <abdullahmohdi1999@gmail.com>
2022-05-07 14:06:20 +0530

    abdullah

---
+++ components/navcheck.js
@@ -0,0 +1,27 @@
+function navbar_main(){
+    return `
+<div id="first">
+    <div id="a">
+        <h6> <a href="index.html">
+                <img src="https://images.ctfassets.net/5de70he6op10/53ZOE4rRqrxcvv0hg2eSLV/a849085e5f600c618132be2475017746/anthro-logo.svg"
+                    alt="">
+            </a>
+        </h6>
+    </div>
+    <div id="b">
+        <h6> <a href="home.html">
+                <img src="https://images.ctfassets.net/5de70he6op10/y1O9tEDOvCGliGUvT9RoJ/eb8ab4734c5e3778b8b61da70ed441ac/AL_NewLogo_2.svg"
+                    alt="">
+            </a>
+        </h6>
+    </div>
+
+</div>
+
+
+
+ `
+}
+
+ export  default navbar_main
+
